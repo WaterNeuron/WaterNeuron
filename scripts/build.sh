35cd704fbcdbf61bb47e18999bea6bd13dcbacaf
EnzoPlayer0ne <enzo@talerdao.org>
2024-07-21 15:22:38 +0200

    save

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,9 @@ set -euxo pipefail
 
 
 MODE="dev"
+ARTIFACTS_DIR="${HOME}/podman_artifacts"
+
+mkdir -p "$ARTIFACTS_DIR"
 
 while [[ $# -gt 0 ]]; do
     case $1 in
@@ -28,6 +31,7 @@ PODMAN_ARGS=(
     --userns=keep-id
     --mount type=bind,source=${HOME},target=${HOME}
     --mount type=bind,source=$(pwd),target=/waterneuron
+    --mount type=bind,source=${ARTIFACTS_DIR},target=/artifacts
     "$PODMAN_HASH"
 )
 
@@ -35,7 +39,9 @@ if [[ "$MODE" == "build" ]]; then
     PODMAN_ARGS+=(
         /usr/bin/bash 
         -c 
-        "bazel build ... && sha256sum bazel-bin/water_neuron/canister_shrink.wasm.gz"
+        "bazel build ... --action_env=GIT_COMMIT_ID=$(git rev-parse HEAD) && \
+            sha256sum bazel-bin/water_neuron/canister_shrink.wasm.gz && \
+            cp bazel-bin/water_neuron/canister_shrink.wasm.gz /artifacts/"
     )
 fi
 
