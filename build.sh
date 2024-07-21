4ba97e3a9ff871852e0db1a9acf1cd51aba00c6e
Your Name <you@example.com>
2024-07-21 11:13:04 +0200

    remove build

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dfx build --network ic water_neuron
-gzip ./.dfx/ic/canisters/water_neuron/water_neuron.wasm --force
-shasum -a 256 ./.dfx/ic/canisters/water_neuron/water_neuron.wasm.gz
