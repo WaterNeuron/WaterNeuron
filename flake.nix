f2b41ad5bbf3eefd402b9e3a9d9b3d001e3ad1d8
EnzoPlayer0ne <enzo@talerdao.org>
2024-08-07 06:56:22 +0000

    save

--- flake.nix
+++ flake.nix
@@ -1,12 +1,10 @@
 {
-  description = "WaterNeuron development environment";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -23,40 +21,37 @@
             cp -r include/* $dev/include/
           '';
         });
-
-        commonPackages = with pkgs; [
-          gcc
-          gnumake
-          binutils
-          coreutils
-          pkg-config
-          openssl
-          libusb1
-          sqlite
-          zlib
-          llvmPackages_18.libclang
-          protobuf
-          llvm
-          lmdb
-          xz
-          shfmt
-          bazel_7
-          bazel-buildtools
-        ];
-
-      in {
-        packages = {
-          libunwind = customLibunwind;
-          default = customLibunwind;
-        };
-
+      in
+      {
         devShells.default = pkgs.mkShellNoCC {
-          packages = commonPackages;
+
+        packages = with pkgs; [
+            gcc
+            gnumake
+            binutils
+            coreutils
+            pkg-config
+            openssl
+            libusb1
+            sqlite
+            zlib
+            llvmPackages_18.libclang
+            protobuf
+            llvm
+            lmdb
+            xz
+            pkg-config
+
+            shfmt
+            customLibunwind
+
+            bazel_7
+            bazel-buildtools
+          ];
 
           shellHook = ''
-            echo "Welcome to WaterNeuron development environment!"
+            echo "Custom development environment loaded"
           '';
         };
-      }
-    );
+      });
 }
