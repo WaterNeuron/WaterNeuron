99be499be88ee1cfb993edf330a2dff0a718c143
EnzoPlayer0ne <enzo@talerdao.org>
2024-10-09 14:25:17 +0000

    sae

--- run.sh
+++ run.sh
@@ -10,10 +10,6 @@ mkdir -p "$ARTIFACTS_DIR"
 
 while [[ $# -gt 0 ]]; do
     case $1 in
-        --dev)
-            MODE="dev"
-            shift
-            ;;
         --build)
             MODE="build"
             shift
@@ -28,7 +24,6 @@ PODMAN_ARGS=(
     --rm
     -w /waterneuron
     --userns=keep-id
-    --mount type=bind,source=${HOME},target=${HOME}
     --mount type=bind,source=$(pwd),target=/waterneuron
     --mount type=bind,source=${ARTIFACTS_DIR},target=/artifacts
     "$PODMAN_HASH"
