4b79230eaabb222e0f159e98ef3b04f75e3731c0
EnzoPlayer0ne <enzo@talerdao.org>
2024-12-14 17:47:25 +0000

    save

--- ic_wasm_utils/src/main.rs
+++ ic_wasm_utils/src/main.rs
@@ -1,8 +1,11 @@
 use serde::{Deserialize, Serialize};
 use std::fs;
 use std::collections::BTreeMap;
+use std::path::PathBuf;
 
-const URL_TEMPLATE: &str = "https://download.dfinity.systems/ic/{}/canisters/{}";
+
+const CANISTER_URL_TEMPLATE: &str = "https://download.dfinity.systems/ic/{version}/canisters/{wasm_file}";
+
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Ord, PartialOrd)]
 #[serde(rename_all = "snake_case")]
@@ -18,6 +21,14 @@ struct CanisterInfo {
     wasm_file: String,
 }
 
+impl CanisterInfo {
+    fn download_url(&self) -> String {
+        CANISTER_URL_TEMPLATE
+            .replace("{version}", &self.version)
+            .replace("{wasm_file}", &self.wasm_file)
+    }
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 struct Config {
     canisters: BTreeMap<CanisterName, CanisterInfo>,
@@ -27,5 +38,11 @@ struct Config {
 fn main() {
     let content = fs::read_to_string("../external_canisters.toml").unwrap();
     let raw_data: Config = toml::from_str(&content).unwrap();
-    dbg!(raw_data);
+    dbg!(&raw_data);
+
+    let cache_dir = dirs::cache_dir()
+        .unwrap_or_else(|| PathBuf::from(".cache"))
+        .join("ic-canisters");
+
+    dbg!(&cache_dir);
 }
