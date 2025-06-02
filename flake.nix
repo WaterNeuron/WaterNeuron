9f59a5ad9d58b735b284424e1ac550370770cb68
EnzoPlayer0ne <enzo@talerdao.org>
2025-06-02 13:27:32 +0000

    add ci

--- flake.nix
+++ flake.nix
@@ -1,26 +1,31 @@
 {
   description = "Reproducible Canisters Environment";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url       = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url   = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ rust-overlay.overlays.default ];
+
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+
         rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
         ic-wasm = pkgs.stdenv.mkDerivation {
-          name = "ic-wasm";
+          name    = "ic-wasm";
           version = "0.9.1";
-          src = pkgs.fetchurl {
-            url = "https://github.com/dfinity/ic-wasm/releases/download/0.9.1/ic-wasm-x86_64-unknown-linux-gnu.tar.gz";
+          src     = pkgs.fetchurl {
+            url    = "https://github.com/dfinity/ic-wasm/releases/download/0.9.1/ic-wasm-x86_64-unknown-linux-gnu.tar.gz";
             sha256 = "sha256-nU2O8sCV82dKaeDH86pj9gOW/01ZnOyZ7P5vnZOPpLE=";
           };
           unpackPhase = ''
@@ -32,19 +37,66 @@
             chmod +x $out/bin/ic-wasm
           '';
         };
+
+        llvmPkgs = pkgs.llvmPackages_18;
+
+        pocketIcVersion = "7.0.0";
+        platform        = if pkgs.stdenv.isDarwin then "darwin" else "linux";
+
+        pocket-ic = pkgs.stdenv.mkDerivation {
+          pname   = "pocket-ic";
+          version = pocketIcVersion;
+          src = pkgs.fetchurl {
+            url =
+              "https://github.com/dfinity/pocketic/releases/download/${pocketIcVersion}/pocket-ic-x86_64-${platform}.gz";
+            # Replace with the real hash (run `nix-prefetch-url --unpack <url>`
+            # once per platform).  This placeholder lets the flake evaluate.
+            sha256 = "sha256-CTXubs4xJxmq5Oq93sLfxq801e2930069TvM0bNjYEQ=";
+          };
+          phases = [ "unpackPhase" "installPhase" ];
+
+          unpackPhase = ''
+            gzip -dc $src > pocket-ic
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            install -m755 pocket-ic $out/bin/
+          '';
+        };
+
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            which
-            curl
-            git
-            gcc
-            wabt
-            ic-wasm
+            # binaries
+            which curl git gcc wabt ic-wasm pkg-config protobuf
+
+            # toolchain
             rustToolchain
+
+            # libraries
+            libunwind lmdb
+
+            llvmPackages_18.clang
+            llvmPkgs.libclang          # the .so that bindgen dlopens
+            stdenv.cc.cc.lib           # brings in libstdc++.so.6
+
+            pocket-ic
           ];
+
           TZ = "UTC";
+
+          # Tell bindgen exactly where libclang lives
+          LIBCLANG_PATH = "${llvmPkgs.libclang.lib}/lib";
+
+          # Make sure the loader can find libstdc++ (and libclang itself)
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            llvmPkgs.libclang
+            pkgs.stdenv.cc.cc.lib      # ← qualified with `pkgs.` (fix)
+          ];
+
+          POCKET_IC_BIN = "${pocket-ic}/bin/pocket-ic";
         };
       });
 }
